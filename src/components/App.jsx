25393e027698941c33b3efb133234b49a08b086f
AndriyMerzlikin <lastheroofme@gmail.com>
2023-12-09 23:07:04 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,25 +2,7 @@ import { ContactsForm } from './ContactsForm/ContactsForm';
 import { Contacts } from './Contacts/Contacts';
 import { Filter } from './Filter/Filter';
 
-// const localStorageKey = 'contacts';
-
-// const getInitialContacts = () => {
-//   const savedContacts = window.localStorage.getItem(localStorageKey);
-//   if (savedContacts !== null) {
-//     return JSON.parse(savedContacts);
-//   }
-//   // return initialContacts;
-// };
-
 export const App = () => {
-  // useEffect(() => {
-  //   window.localStorage.setItem(localStorageKey, JSON.stringify(contacts));
-  // }, [contacts]);
-
-  // const updateFilter = newFilter => {
-  //   setFilter(newFilter);
-  // };
-
   return (
     <div>
       <h1>Phonebook</h1>
